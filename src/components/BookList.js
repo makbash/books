93d8d88ceb9c4b0179afa8a30f4f20be08555d8a
Mustafa Akbaş <makbash21@gmail.com>
2021-06-12 21:13:33 +0300

    REACT CONTEXT API - HOOKS (03)
    Default Value ----> this.context method

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,31 +3,30 @@ import React from 'react';
 import './BookList.css';
 
 import Book from './Book';
+import BookContext from '../contexts/BookContext';
 
 class BookList extends React.Component {
 
+    static contextType = BookContext;
+
     render() {
-        // console.log(this.props.books)
+        // console.log(this.context)
 
-        // return this.props.books.map((book, i) => {
-        //     return <div key={i}>
-        //         <Book book={book} />
-        //     </div>
-        // })
-
-        const bookList = this.props.books.map((book, i) => {
-            return <Book book={book} key={i} />
-        })
+        const books = this.context;
 
         return (
             <section className="page-section bg-light" id="portfolio">
                 <div className="container">
                     <div className="text-center">
-                        <h2 className="section-heading text-uppercase">Bookfolio</h2>
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
                         <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                     </div>
                     <div className="row">
-                        {bookList}
+                        {
+                            books.map((book, i) => {
+                                return <Book book={book} key={i} />
+                            })
+                        }
                     </div>
                 </div>
             </section>
